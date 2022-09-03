af11121ed2503cbdbb60333510b4ae77598d0ee2
[anishgupta550] <[anishgupta550@gmail.com]>
2022-09-03 08:11:18 +0530

    real estate website

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// let scoll_color = document.getElementById("scroll");
+
+// window.onscroll = function () {
+//     scrollfunc();
+// }
+
+// function scrollfunc() {
+//     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         scoll_color.style.backgroundColor = "rgb(0,0,0,0.8)";
+//         scoll_color.style.transition = "0.5s";
+        
+//     }else{
+//         scoll_color.style.backgroundColor = "rgb(0,0,0,0)";
+//     }
+// }
+
+
